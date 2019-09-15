2e4cc6cc43eba1d625601b17dbce2ecd198e92ed
HarchevnikovDenis <harchevnikovdenis@mail.ru>
2019-09-15 20:02:34 +0300

    init commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") != 0)
+            Movement(Input.GetAxis("Horizontal"));
+
+    }
+
+    void Movement(float dir)
+    {
+        rb.AddForce(new Vector2(dir * speed, 0), ForceMode2D.Force);
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# ball Game
+<br>
+The game simulates the physics of a basketball on Earth and other planets
