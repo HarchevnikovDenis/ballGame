10d5b66ebbd20fee36d343a4ef538d15dfd8570d
HarchevnikovDenis <harchevnikovdenis@mail.ru>
2019-09-15 20:29:33 +0300

    the Earth scene is ready

---
+++ Assets/Scripts/PlatformColorChanging.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlatformColorChanging : MonoBehaviour
+{
+    private SpriteRenderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        rend.material.color = new Color(Random.value, Random.value, Random.value, 1.0f);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            Color randomColor = new Color(Random.value, Random.value, Random.value, 1.0f);
+            rend.material.color = randomColor; 
+        }
+    }
+}
